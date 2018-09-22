936ca9f254ab47239b27b485b6f7f43fcf70b889
Mayursinh <mayursiinh@gmail.com>
2018-09-22 15:24:45 +0530

    Added left and right keypress events

--- public/main.js
+++ public/main.js
@@ -1,5 +1,36 @@
+function slide_page_right() 
+{
+    $('.user-id').css('left', '0');
+    $('.arrow-right').css('right', '100vw');
+}
+
+function slide_page_left()
+{
+    $('.user-id').css('left', '100%');
+       $('.arrow-right').animate({
+           right : "20px"
+       }, 1000);
+}
+
 $(document).ready(function(){
     $('body').show(1000);
+ 
+   //key left and right
+   let user_form_section = 1;
+   $(document).keydown((e) => {
+       let keycode = e.keyCode;
+       if(keycode == 39 && user_form_section == 1)
+       {
+            slide_page_right();
+            user_form_section = 0;
+       }
+       if(keycode == 37 && user_form_section == 0)
+       {
+           slide_page_left();
+           user_form_section = 1;
+       }
+   });
+
 
     //show and hide password
    $('.eyes').hover(() => {
@@ -19,14 +50,10 @@ $(document).ready(function(){
 
    //page slide
    $('.arrow-right').click(() => {
-       $('.user-id').css('left', '0');
-       $('.arrow-right').css('right', '100vw');
+       slide_page_right();
    });
    $('.arrow-left').click(() => {
-       $('.user-id').css('left', '100%');
-       $('.arrow-right').animate({
-           right : "20px"
-       }, 1000);
+       slide_page_left();
    });
 
 });
